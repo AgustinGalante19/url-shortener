af0a39dd3f269e49caeee74080c85b928df56f5e
Agustin Galante <agustin.galante.19@outlook.es>
2024-01-14 22:51:41 -0300

    Basic short url creation

---
+++ API/server.js
@@ -0,0 +1,50 @@
+import Fastify from "fastify"
+import client from "./src/client/prisma.js"
+import ShortUniqueId from "short-unique-id"
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.get("/", () => {
+  return "Welcome to url shortener by Agustin Galante"
+})
+
+fastify.post("/shortUrl", async (req) => {
+  const { fullUrl } = req.body
+  const shortUrl = new ShortUniqueId({ length: 10 }).rnd()
+  await client.url.create({
+    data: {
+      fullUrl,
+      shortUrl,
+    },
+  })
+
+  return { method: "post", result: shortUrl }
+})
+
+fastify.get("/:shortUrl", async (req, reply) => {
+  try {
+    const shortUrl = await client.url.findMany({
+      where: {
+        shortUrl: {
+          equals: req.params.shortUrl,
+        },
+      },
+    })
+    if (shortUrl.length === 0) return reply.status(404).send("not found")
+    return reply.redirect(shortUrl.at(0).fullUrl)
+  } catch (err) {
+    return "error"
+  }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
