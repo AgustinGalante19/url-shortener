14dadb6e53747d260d17008f8fdfb915eb52ab13
Agustin Galante <agustin.galante.19@outlook.es>
2024-01-15 12:26:09 -0300

    Default .env port

--- API/server.js
+++ API/server.js
@@ -42,7 +42,7 @@ fastify.get("/:shortUrl", async (req, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: process.env.PORT || 3000 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
